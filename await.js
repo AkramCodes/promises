30cfe04105cae24868252cb51ee8dd4dfc4d72e7
AkramCodes <sbntech72@gmail.com>
2024-01-23 15:21:01 +0530

    first commit

---
+++ await.js
@@ -0,0 +1,22 @@
+
+
+
+
+// https://dummyjson.com/users
+// https://dummyjson.com/posts
+
+async function fetchMyData(url) {
+    let data = await fetch(url)
+    let done = await data.json()
+    return new Promise(function(resolve,reject){
+        resolve(done)
+    })
+
+}
+
+fetchMyData('https://dummyjson.com/users').then(function(success){
+    console.log(success)
+
+},function(error){
+
+})
---
+++ promise.js
@@ -0,0 +1,17 @@
+
+
+// let promis = new Promise(function(resolve,reject){
+//     let data = fetch('https://dummyjson.com/users').then(Response=>Response.json()).then((data)=>{return data.users[1]})
+//     resolve(data)
+//     reject()
+
+
+// })
+
+// promis.then(function(success){
+//     console.log(success)
+
+// },function(error){
+//     console.log(error)
+
+// })
